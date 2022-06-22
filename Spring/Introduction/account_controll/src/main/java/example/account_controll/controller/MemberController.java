0630406ff34dbd91c8f7106c61c5f245ee32bf45
stbaker517 <stbaker517@naver.com>
2022-06-22 12:31:38 +0900

    self member signup test and show user list test

--- Spring/Introduction/account_controll/src/main/java/example/account_controll/controller/MemberController.java
+++ Spring/Introduction/account_controll/src/main/java/example/account_controll/controller/MemberController.java
@@ -1,8 +1,14 @@
 package example.account_controll.controller;
 
+import example.account_controll.domain.Member;
 import example.account_controll.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,4 +18,23 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    //self test
+    @GetMapping("signup")
+    @ResponseBody
+    public Long postSignUpAPI(@RequestParam("name") String name){
+        Member member = new Member();
+        member.setName(name);
+        Long id = memberService.join(member);
+        return id;
+    }
+
+    @GetMapping("userlist")
+    @ResponseBody
+    public List<Member> getUserListAPI(){
+        return memberService.findMembers();
+
+    }
+
+
 }
